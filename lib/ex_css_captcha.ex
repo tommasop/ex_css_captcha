1b61390ed937f5e2e759c41fe61b86f8462ad784
Tommaso Patrizi <tommasop@gmail.com>
2020-01-06 16:04:22 +0100

    added validation for params forms (NO ECTO)

--- lib/ex_css_captcha.ex
+++ lib/ex_css_captcha.ex
@@ -9,15 +9,15 @@ defmodule ExCSSCaptcha do
     noise_length: 2,
     challenge_length: 8,
     fake_characters_length: 2,
-    #fake_characters_color: nil,
+    # fake_characters_color: nil,
     significant_characters_color: nil,
     html_wrapper_id: :captcha,
     html_letter_tag: :span,
     html_wrapper_tag: :div,
     unicode_version: :ascii,
-    #fake_characters_style: "display: none",
+    # fake_characters_style: "display: none",
     significant_characters_style: "",
-    renderer: ExCSSCaptcha.DefaultRenderer,
+    renderer: ExCSSCaptcha.DefaultRenderer
   ]
 
   def options(options) do
@@ -29,10 +29,13 @@ defmodule ExCSSCaptcha do
 
   # TODO: move this to defaults/config
   @separator "/"
-  @expires_in 300 # in seconds
+  # in seconds
+  @expires_in 300
   @algorithm "AES128GCM"
-  @key :crypto.strong_rand_bytes(32) # (re)generated at compile time
-  @pepper :crypto.strong_rand_bytes(24) # (re)generated at compile time
+  # (re)generated at compile time
+  @key :crypto.strong_rand_bytes(32)
+  # (re)generated at compile time
+  @pepper :crypto.strong_rand_bytes(24)
   def encrypt(content) do
     iv = :crypto.strong_rand_bytes(32)
     {ct, tag} = :crypto.block_encrypt(:aes_gcm, @key, iv, {@algorithm, content})
@@ -60,8 +63,7 @@ defmodule ExCSSCaptcha do
   def random(n, n), do: n
 
   def random(n1, n2)
-    when is_integer(n1) and is_integer(n2)
-  do
+      when is_integer(n1) and is_integer(n2) do
     :rand.uniform(n2 - n1 + 1) + n1 - 1
   end
 
@@ -70,25 +72,28 @@ defmodule ExCSSCaptcha do
   end
 
   def encrypt_and_sign(challenge) do
-    content = [@pepper, challenge, DateTime.utc_now()]
-    |> Enum.join(@separator)
-    hash = content
-    |> digest()
+    content =
+      [@pepper, challenge, DateTime.utc_now()]
+      |> Enum.join(@separator)
+
+    hash =
+      content
+      |> digest()
+
     [content, hash]
     |> Enum.join(@separator)
     |> encrypt()
   end
 
   @length 32
-  [captcha, captcha2] = 1..2
-  |> Enum.map(
-    fn _ ->
+  [captcha, captcha2] =
+    1..2
+    |> Enum.map(fn _ ->
       @length
       |> :crypto.strong_rand_bytes()
       |> Base.url_encode64()
       |> binary_part(0, @length)
-    end
-  )
+    end)
 
   def bypass_captcha(params) do
     params
@@ -96,17 +101,56 @@ defmodule ExCSSCaptcha do
     |> Map.put("captcha2", unquote(captcha2))
   end
 
-  def validate_captcha(changeset = %Ecto.Changeset{valid?: false}), do: changeset
-  def validate_captcha(changeset = %Ecto.Changeset{params: %{"captcha" => unquote(captcha), "captcha2" => unquote(captcha2)}}), do: changeset
-  def validate_captcha(changeset = %Ecto.Changeset{params: %{"captcha" => captcha1, "captcha2" => captcha2}}) do
+  def validate_captcha(params = %{"captcha" => captcha1, "captcha2" => captcha2}) do
     import ExCSSCaptcha.Gettext
     captcha1 = String.downcase(captcha1)
+
     with(
       {:ok, data} when is_binary(data) <- decrypt(captcha2),
       [@pepper, ^captcha1, datetime, hash] <- String.split(data, @separator),
-      ^hash <- [@pepper, captcha1, datetime]
-      |> Enum.join(@separator)
-      |> digest(),
+      ^hash <-
+        [@pepper, captcha1, datetime]
+        |> Enum.join(@separator)
+        |> digest(),
+      {:ok, datetime, 0} <- DateTime.from_iso8601(datetime)
+    ) do
+      if DateTime.diff(DateTime.utc_now(), datetime) > @expires_in do
+        require Logger
+        Logger.debug("captcha expired")
+        false
+      else
+        true
+      end
+    else
+      value ->
+        require Logger
+        Logger.debug("captcha validation failed with: #{inspect(value)}")
+        false
+    end
+  end
+
+  def validate_captcha(changeset = %Ecto.Changeset{valid?: false}), do: changeset
+
+  def validate_captcha(
+        changeset = %Ecto.Changeset{
+          params: %{"captcha" => unquote(captcha), "captcha2" => unquote(captcha2)}
+        }
+      ),
+      do: changeset
+
+  def validate_captcha(
+        changeset = %Ecto.Changeset{params: %{"captcha" => captcha1, "captcha2" => captcha2}}
+      ) do
+    import ExCSSCaptcha.Gettext
+    captcha1 = String.downcase(captcha1)
+
+    with(
+      {:ok, data} when is_binary(data) <- decrypt(captcha2),
+      [@pepper, ^captcha1, datetime, hash] <- String.split(data, @separator),
+      ^hash <-
+        [@pepper, captcha1, datetime]
+        |> Enum.join(@separator)
+        |> digest(),
       {:ok, datetime, 0} <- DateTime.from_iso8601(datetime)
     ) do
       if DateTime.diff(DateTime.utc_now(), datetime) > @expires_in do
